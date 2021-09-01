17be15fce5497ed8b240db9793d17258d4928fb5
user <user@DESKTOP-ILC1840>
2021-09-01 17:23:42 +0900

    0901

---
+++ school/src/main/java/egovframework/let/temp/service/impl/TempServiceImpl.java
@@ -0,0 +1,34 @@
+package egovframework.let.temp.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import egovframework.let.temp.service.TempService;
+import egovframework.let.temp.service.TempVO;
+import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import egovframework.rte.fdl.property.EgovPropertyService;
+
+@Service("tempService")
+public class TempServiceImpl extends EgovAbstractServiceImpl implements TempService {
+
+	@Resource(name = "propertiesService")
+	protected EgovPropertyService propertyService;
+	/*
+	@Resource(name = "tempMapper")
+	private TempMapper tempMapper;
+	*/
+	@Resource(name = "tempDAO")
+	private TempDAO tempDAO;
+	/*
+	@Override
+	public TempVO selectTemp(TempVO vo) throws Exception {
+		return tempMapper.seletTemp(vo);
+	}
+	*/
+
+	@Override
+	public TempVO selectTemp(TempVO vo) throws Exception {
+		return tempDAO.selectTemp(vo);
+	}
+}
